3f90d26733d265038e40ed8656f012f6618a4291
Andrey Chernykh <andrei.chernykh@gmail.com>
2018-11-06 13:48:53 +0200

    init

---
+++ lib/exop_props.ex
@@ -0,0 +1,18 @@
+defmodule ExopProps do
+  @moduledoc """
+  Documentation for ExopProps.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExopProps.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exop_props_test.exs
@@ -0,0 +1,8 @@
+defmodule ExopPropsTest do
+  use ExUnit.Case
+  doctest ExopProps
+
+  test "greets the world" do
+    assert ExopProps.hello() == :world
+  end
+end
