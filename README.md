def19f78d39077d4dcbe385f639c0a70b9c80b06
Andrey Chernykh <andrei.chernykh@gmail.com>
2018-11-06 15:12:11 +0200

    mix + readme + changelog

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
 # ExopProps
 
-**TODO: Add description**
-
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `exop_props` to your list of dependencies in `mix.exs`:
-
-```elixir
-def deps do
-  [
-    {:exop_props, "~> 0.1.0"}
-  ]
-end
-```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/exop_props](https://hexdocs.pm/exop_props).
-
+looks promising 💎
--- mix.exs
+++ mix.exs
@@ -1,26 +1,46 @@
-defmodule ExopProps.MixProject do
+defmodule ExopProps.Mixfile do
   use Mix.Project
+
+  @description """
+  Here will be a description
+  """
 
   def project do
     [
       app: :exop_props,
       version: "0.0.0",
       elixir: "~> 1.3",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      name: "ExopProps",
+      description: @description,
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/madeinussr/exop_props",
+      docs: [extras: ["README.md"]],
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod
     ]
   end
 
   def application do
     [
-      extra_applications: [:logger]
+      applications: [:logger, :stream_data]
     ]
   end
 
   defp deps do
     [
+      {:ex_doc, "~> 0.12", only: [:dev, :test, :docs]},
       {:stream_data, "~> 0.1"},
       {:exop, "~> 1.1.4"}
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Andrey Chernykh", "Aleksandr Fomin"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/madeinussr/exop_props"}
     ]
   end
 end
