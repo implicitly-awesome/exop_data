4502764f90650ac1e2b95715818572bd9a940af4
Andrey Chernykh <andrei.chernykh@gmail.com>
2018-11-09 10:30:13 +0200

    integer generator specs + numericality aliases

--- lib/exop_props/params_generator/integer.ex
+++ lib/exop_props/params_generator/integer.ex
@@ -11,11 +11,27 @@ defmodule ExopProps.ParamsGenerator.Integer do
     opts |> Map.get(:numericality) |> do_generate()
   end
 
+  @spec do_generate(map()) :: StreamData.t()
+  @spec do_generate(map()) :: StreamData.t()
   defp do_generate(%{equal_to: exact}), do: constant(exact)
 
   defp do_generate(%{equals: exact}), do: constant(exact)
 
   defp do_generate(%{is: exact}), do: constant(exact)
+
+  defp do_generate(%{min: min} = contract) do
+    contract
+    |> Map.delete(:min)
+    |> Map.put(:greater_than_or_equal_to, min)
+    |> do_generate()
+  end
+
+  defp do_generate(%{max: max} = contract) do
+    contract
+    |> Map.delete(:max)
+    |> Map.put(:less_than_or_equal_to, max)
+    |> do_generate()
+  end
 
   defp do_generate(%{min: min} = contract) do
     contract
@@ -65,7 +81,9 @@ defmodule ExopProps.ParamsGenerator.Integer do
 
   defp do_generate(_), do: StreamData.integer()
 
+  @spec constant(integer()) :: StreamData.t()
   defp constant(value), do: StreamData.constant(value)
 
+  @spec in_range(integer(), integer()) :: StreamData.t()
   defp in_range(min, max), do: StreamData.integer(min..max)
 end
